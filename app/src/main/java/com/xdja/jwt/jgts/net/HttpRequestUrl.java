cfb11309bca078118b72546082d07b2cf53aa86b
gouhao <gouhao@xdja.com>
2017-03-21 17:31:53 +0800

    增加请求地址文件

---
+++ app/src/main/java/com/xdja/jwt/jgts/net/HttpRequestUrl.java
@@ -0,0 +1,21 @@
+package com.xdja.jwt.jgts.net;
+
+/**
+ * Created by gouhao on 3/21/2017.
+ */
+
+public class HttpRequestUrl {
+    private static final String REQUEST_LOGIN = "";
+    private static final String REQUEST_INSERT = "";
+    private static final String REQUEST_UPDATE = "";
+    private static final String REQUEST_QUERY = "";
+    private static final String REQUEST_FEEDBACK = "";
+
+    private static final String REQUEST_RECEIVE = "";
+    private static final String REQUEST_FEFUSE = "";
+    private static final String REQUEST_LEAVE = "";
+    private static final String REQUEST_ARRIVE = "";
+    private static final String REQUEST_FINISH = "";
+
+
+}
